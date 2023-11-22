94367d0024238b6f7e29abacb86c60195651c0b1
thegove <gove.allen@gmail.com>
2023-11-22 09:09:46 -0700

    building infrastructure

--- system.js
+++ system.js
@@ -1,19 +1,21 @@
-const INITIAL_FUNCTION = start_me_up
+const GLOBALS={
+  blogger:false
+}
 
 const PAGE_URL = new URL(window.location)
-async function start_me_up(){
+async function initialize(platform="web"){
+    GLOBALS.blogger=platform==="blogger"
+
     console.log("starting")
     const url_params = get_params()
     console.log("url_params",url_params)
-    let page_path="src/index.html"
+    let page_path=null
     if(url_params.page){
-        page_path = "src/" + url_params.page
+      page_path = url_params.page
+    }else{
+      page_path="content/index.html"
     }
-    const page=await load_page(page_path)
-    console.log("page", page)
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(page, "text/html")
-    document.body.replaceChildren(doc.body)
+    await load_page(page_path)
     
 }
 
@@ -21,24 +23,78 @@ async function start_me_up(){
 async function get_url(page_path) {
     const PAGE_URL = new URL(window.location)
 
-    if (SERVER==="blogger") {
+    if (GLOBALS.blogger) {
       return `${PAGE_URL.protocol}//${PAGE_URL.host}/2022/02/${await bloggerId(page_path)}.html`
     } else {
       return page_path
     }
   }
+function blog_link_handler(evt){
+  console.log("at link handler", evt.target.getAttribute("href"))
+  const link=evt.target.getAttribute("href")
+  if(link.includes(":")){
+    // this is a link with a protocol. just follow it
+    return true
+  }else if(link.startsWith("/")){
+    //this is a absolute path on this site
+    load_page(link.substring(1))// get rid of the initial slash because we don't prefix with slash in pageId
+  }else{
+    // must be a relative path on this site
+    // need to have a way to figure the path of the current page
+    const current_path=document.body.dataset.pagePath.split("/")
+    current_path.pop()
+    new_path=link.split("/")
 
+    while(new_path[0]==="."){      
+      new_path.shift()
+    }
+
+    while(new_path[0]===".."){
+      new_path.shift()
+      current_path.pop()
+    }
+    
+    console.log("loading relative link not yet supported", current_path, new_path, ) 
+    
+    load_page(current_path.concat(new_path).join("/"))
+  }
+  evt.preventDefault();
+  return false
+  
+}
+
+async function get_page_content(page_path){
+  const url = await get_url(page_path)
+  console.log("url", url)
+  const response = await fetch(url)
+  return await response.text();  
+
+}
 
 async function load_page(page_path){
-    const url = await get_url(page_path)
-    console.log("url", url)
-    const response = await fetch(url)
-    let post_body = await response.text();  
-    if (SERVER==="blogger") {
+    let raw_page = await get_page_content(page_path);  
+    const parser = new DOMParser();
+    if (GLOBALS.blogger) {
         const contentDelimiter="==~~--FiLe"+"-"+"CoNtEnTs--~~=="
-        return post_body.split(contentDelimiter)[1]
+        raw_page = raw_page.split(contentDelimiter)[1]
     }
-    return post_body
+
+    const doc = parser.parseFromString(raw_page, "text/html")
+
+    if (GLOBALS.blogger || true) {
+      // add event listener to links on blog
+      for(const element of doc.getElementsByTagName('a')) {
+          console.log("link handler", element)
+          element.onclick = blog_link_handler
+      }
+
+    }
+
+    console.log("doc",doc)
+    doc.body.dataset.pagePath=page_path
+    document.body.parentNode.replaceChildren(doc.body)
+    
+
 
 }
 
