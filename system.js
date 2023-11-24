0e9e3298d09237bf779cfef18d475bfc2afac67b
thegove <gove.allen@gmail.com>
2023-11-24 16:47:17 -0700

    interface progress

--- system.js
+++ system.js
@@ -5,63 +5,29 @@ const GLOBALS={
 
 async function initialize(platform="web"){
     GLOBALS.blogger=platform==="blogger"
-
-
-
+    console.log("document",document)
+    //google material icons
     add_to_head("link",{
       id:"google-fonts",
       href:'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
       rel:'stylesheet'
     })
-  
-    // add_to_head("script",{
-    //   id:"prism-1",
-    //   src:'https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/components/prism-core.min.js'
-    // })
-  
-    // add_to_head("script",{
-    //   id:"prism-2",
-    //   src:"https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/autoloader/prism-autoloader.min.js"
-    // })
-  
-    // add_to_head("script",{
-    //   id:"sqlite",
-    //   crossorigin:'anonymous',
-    //   integrity:'sha512-n7swEtVCvXpQ7KxgpPyp0+DQQEf5vPpmzCRl14x2sp2v5LpCYCjrAx03mkBAbWwGF9eUoIk8YVcDFuJRIeYttg==',
-    //   referrerpolicy:'no-referrer',
-    //   src:'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.js'
-    // })
-  
-    add_to_head("script",{
-      id:'markdown',
-      crossorigin:'anonymous',
-      integrity:'sha512-uwSxMaa/W7dmSIXEd07BMVymisMRRUS/Pr5a76AquekKKu9HWn4rBiCd+ZtwqnoijAJvttdrz8krzP26kZjg0Q==',
-      referrerpolicy:'no-referrer',
-      src:'https://cdnjs.cloudflare.com/ajax/libs/marked/4.2.12/marked.min.js'
-    })
-  
-    // add_to_head("script",{
-    //   id:"sjcl",
-    //   crossorigin:'anonymous',
-    //   integrity:'sha512-s0r9TyYSs5CusSDL5s/QEZQZ4DNYLDXx0S+imS+SHR9mw0Yd0Z5KNS9dw7levSp7GpjOZu/bndds3FEDrKd8dg==',
-    //   referrerpolicy:'no-referrer',
-    //   src:'https://cdnjs.cloudflare.com/ajax/libs/sjcl/1.0.8/sjcl.min.js'
-    // })
-  
-    load_js_from_blog_post("firebase-js","module")
-  
 
 
+    // ------------  load interface  ------------------
+    load_css("interface/interface.css")
+    load_js("interface/interface.js")
 
 
     console.log("starting")
     const url_params = get_params()
     console.log("url_params",url_params)
+
     let page_path=null
     if(url_params.page){
       page_path = url_params.page
     }else{
-      page_path="content/index.html"
+      page_path="interface/index.html"
     }
     await load_page(page_path)
     
@@ -80,7 +46,7 @@ async function get_url(page_path) {
 function blog_link_handler(evt){
   console.log("at link handler", evt.target.getAttribute("href"))
   const link=evt.target.getAttribute("href")
-  if(link.includes(":")){
+  if(link.split("?")[0].includes(":")){
     // this is a link with a protocol. just follow it
     return true
   }else if(link.startsWith("/")){
@@ -117,28 +83,35 @@ async function get_page_content(page_path){
 
 }
 
-async function load_page(page_path){
+async function load_page(page_path, destination_tag_or_tag_id){
     let raw_page = await get_page_content(page_path);  
     const parser = new DOMParser();
     if (GLOBALS.blogger) {
         const contentDelimiter="==~~--FiLe"+"-"+"CoNtEnTs--~~=="
         raw_page = raw_page.split(contentDelimiter)[1]
+        raw_page = decodeHtml(raw_page)
     }
 
     const doc = parser.parseFromString(raw_page, "text/html")
 
-    if (GLOBALS.blogger || true) {
+    //if (GLOBALS.blogger || true) {
       // add event listener to links on blog
-      for(const element of doc.getElementsByTagName('a')) {
-          console.log("link handler", element)
-          element.onclick = blog_link_handler
-      }
-
+    for(const element of doc.getElementsByTagName('a')) {
+        console.log("link handler", element)
+        element.onclick = blog_link_handler
     }
 
+    // }
+
     console.log("doc",doc)
-    doc.body.dataset.pagePath=page_path
-    document.body.parentNode.replaceChildren(doc.body)
+    if(document.body){
+      // the document has a body tag, replace current body with it
+      doc.body.dataset.pagePath=page_path
+      const head=document.head
+      document.body.parentNode.replaceChildren(head, doc.body)
+    }else{
+      tag(destination_tag_or_tag_id).replaceChildren(doc)
+    }
     
 
 
@@ -177,3 +150,82 @@ function get_params(query_string) {
   return url_params
 }
   
+
+/** 
+ * gets js that is stored as a base-64 encoded string (if fetched in production) 
+ * in a post and loads it to the <header> tag.
+ * If the tag already exists, then it will override the js tag with the content of the file. 
+ * @param {string} web_path the name of the file we want to get. It gets converted into 
+ * a URL.
+ */
+async function load_js(web_path, script_type) {
+  const response = await fetch(await get_url(web_path))
+  let source = await response.text();
+  if (GLOBALS.blogger) {source = atob(source)}
+  js_tag_id=web_path.split("/").join("-")
+  let js_tag = document.getElementById(js_tag_id)
+
+
+  if (!js_tag) {
+    js_tag = document.createElement(`script`)
+    js_tag.setAttribute("id", js_tag_id)
+    if(script_type){
+      js_tag.setAttribute("type", script_type)
+    }else{
+      const type_attr = source.match(/@type=["'](.*)['"]@/)
+      if (type_attr) {
+        js_tag.setAttribute("type", type_attr[1])
+      }
+    }
+    js_tag.async = false;
+    document.getElementsByTagName('head')[0].appendChild(js_tag)
+  }
+  js_tag.addEventListener('load', function() {
+    //log("hi hi hi");
+  });
+
+  js_tag.replaceChildren(source)
+}
+
+
+
+async function add_to_head(tag_name, attributes) {
+  // place a tag in the document header with the specified attributes
+  let tag_id_header = document.getElementById(attributes.id)
+  if (!tag_id_header) {
+    tag_id_header = document.createElement(tag_name)
+    for(const [attr_name, attr_val] of Object.entries(attributes)){
+      tag_id_header.setAttribute(attr_name, attr_val)
+    }
+    document.head.appendChild(tag_id_header)
+  }
+}
+
+function htmlDecode(input) {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
+}
+
+// send a tag or the id of the tag.  returns the relevant tag
+function tag(tag_or_id) {
+  if(typeof tag_or_id === 'string'){
+    return document.getElementById(tag_or_id)
+  }else{
+    return tag_or_id
+  }
+}
+
+
+async function load_css(web_path) {
+  // gets css that is stored in a post and load it
+
+  const response = await fetch(await get_url(web_path))
+  let source = await response.text();
+  if(GLOBALS.blogger){
+    source = decodeHtml(post_body)
+  }
+  
+  const css = document.createElement(`style`)
+  css.innerHTML = source
+  document.head.appendChild(css)
+}
